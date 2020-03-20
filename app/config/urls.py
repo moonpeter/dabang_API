765d744240fb811c15760fac61c28cf1038cd9f0
Jeon Young Hoon <hungyb0924@gmail.com>
2020-03-20 15:54:23 +0900

    social login - kakao #1

--- app/config/urls.py
+++ app/config/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from members import views
+from members.urls import urlpatterns_members
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('members/', include(urlpatterns_members)),
+    path('login/', views.login_page, name='login-page'),
 ]
