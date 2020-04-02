23bfcf493b12677551bbdeddf81fc9e83e5ad86b
Jeon Young Hoon <hungyb0924@gmail.com>
2020-04-03 08:15:24 +0900

    dabang crawling test #1

--- app/members/admin.py
+++ app/members/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 
 from members.models import SocialLogin
-from posts.models import SalesForm
+from posts.models import SalesForm, ControlPoint
 
 User = get_user_model()
 
@@ -25,6 +25,11 @@ class SalesFormAdmin(admin.ModelAdmin):
     list_display = ['pk', 'type']
 
 
+class ControlPointAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name' ]
+
+
 admin.site.register(User, UserAdmin)
 admin.site.register(SocialLogin, SocialLoginAdmin)
 admin.site.register(SalesForm, SalesFormAdmin)
+admin.site.register(ControlPoint, ControlPointAdmin)
