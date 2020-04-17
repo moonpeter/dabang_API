20f750c5626d7fb605fc3d0747a89697c2643722
Jeon Young Hoon <hungyb0924@gmail.com>
2020-04-18 04:33:26 +0900

    소셜로그인 코드 수정 - 한 view 안에서 모든 소셜 서비스 처리하도록

--- app/members/urls.py
+++ app/members/urls.py
@@ -2,20 +2,20 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from members import apis, views
-from members.apis import KakaoSignInCallbackView, testSoical
+from members.apis import KakaoSignInCallbackView, socialLogin
 
 router = DefaultRouter()
 router.register(r'user', apis.UserModelViewSet, basename='UserModel')
 
 urlpatterns_members = [
     path('kakaoToken/', apis.KakaoJwtTokenView.as_view()),
-        path('facebookToken/', apis.FacebookJwtToken.as_view()),
+    path('facebookToken/', apis.FacebookJwtToken.as_view()),
 
     path('kakaoTest/', apis.KAKAO.as_view()),
     path('sign-in/kakao/callback/', KakaoSignInCallbackView.as_view()),
 
     path('kakao-login/', views.kakao_login),
-    path('test/', testSoical.as_view()),
+    path('socialLogin/', socialLogin.as_view()),
 
 ]
 urlpatterns_members += router.urls
